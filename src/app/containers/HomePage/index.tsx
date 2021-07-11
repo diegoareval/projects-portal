c15197a17c3672d2eda91d43739660f69205f0c5
Diego <diego2000avelar@gmail.com>
2021-07-10 18:13:41 -0600

    navs

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from "styled-components";
+import tw from "twin.macro";
+import { Navbar } from '../../components/Navbar';
+const PageContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden
+  `}
+`;
+
+const index = () => {
+    return (
+        <PageContainer>
+           <Navbar />
+        </PageContainer>
+    )
+}
+
+export default index
