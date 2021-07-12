e04cc8d42bb4b6160601eea6c945e42172f4b05f
Diego <diego2000avelar@gmail.com>
2021-07-11 19:21:47 -0600

    contact form

--- src/app/containers/contact/index.tsx
+++ src/app/containers/contact/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Navbar } from '../../components/Navbar';
+import { Button } from "../../components/button";
+import Social from '../../components/social';
+
 
 const PageContainer = styled.div`
   ${tw`
@@ -25,11 +28,76 @@ const Title = styled.h1`
   `};
 `;
 
+const Section = styled.section`
+  ${tw`
+  text-gray-700  relative
+  `};
+`;
+
 const Contact = () => {
   return (
     <PageContainer>
       <Navbar />
-       <Title>Contact</Title>
+      <Title>Contact</Title>
+      <Section className="text-gray-700 body-font relative">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+             Please Leave a comments a we gonna get back yo you right away
+            </p>
+          </div>
+          <div className="lg:w-1/2 md:w-2/3 mx-auto">
+            <div className="flex flex-wrap -m-2">
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label className="leading-7 text-sm text-gray-600">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <div className="relative">
+                  <label
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <Button text="Submit" />
+              </div>
+             <Social/>
+            </div>
+          </div>
+        </div>
+      </Section>
     </PageContainer>
   )
 }
